af3199fe2e8d398ebb2b6427dcc914c39eb5a3b8
mba <>
2021-07-05 01:01:12 +0300

    fx

---
+++ iBase/iBase/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iBase
+//
+//  Created by Melike Büşra Ayhan on 5.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
