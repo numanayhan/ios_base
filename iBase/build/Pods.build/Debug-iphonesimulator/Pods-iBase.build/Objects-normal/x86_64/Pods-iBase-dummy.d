7bb6e7016c30ccb45933d2feb959d5bef6b7d6f5
mba <>
2021-07-05 09:20:34 +0300

    ios 11 fixed

---
+++ iBase/build/Pods.build/Debug-iphonesimulator/Pods-iBase.build/Objects-normal/x86_64/Pods-iBase-dummy.d
@@ -0,0 +1,5 @@
+dependencies: \
+  /Users/melikebusraayhan/Repository/ios_base/iBase/Pods/Target\ Support\ Files/Pods-iBase/Pods-iBase-dummy.m \
+  /Users/melikebusraayhan/Repository/ios_base/iBase/build/Pods.build/Debug-iphonesimulator/Pods-iBase.build/module.modulemap \
+  /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator14.5.sdk/usr/include/mach-o/compact_unwind_encoding.modulemap \
+  /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator14.5.sdk/usr/include/mach-o/dyld.modulemap
---
+++ iBase/build/Pods.build/Debug-iphonesimulator/Pods-iBase.build/Objects-normal/x86_64/Pods_iBase_vers.d
@@ -0,0 +1,2 @@
+dependencies: \
+  /Users/melikebusraayhan/Repository/ios_base/iBase/build/Pods.build/Debug-iphonesimulator/Pods-iBase.build/DerivedSources/Pods_iBase_vers.c
