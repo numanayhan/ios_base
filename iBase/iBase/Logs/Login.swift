03b483aae176f6d612cfd812bb229717ac99e62b
mba <>
2021-07-07 01:20:56 +0300

    logs added

---
+++ iBase/iBase/Logs/Login.swift
@@ -0,0 +1,15 @@
+//
+//  Login.swift
+//  iBase
+//
+//  Created by Melike Büşra Ayhan on 5.07.2021.
+//
+
+import Foundation
+
+
+class LoginLog: NSObject {
+    func errorLogin(msg:String) -> String {
+        return msg 
+    }
+}
